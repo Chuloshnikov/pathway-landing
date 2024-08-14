8c76be4b555212a0e95b718b6e8329f19ae04756
MaxChuloshnikov <89795931+Chuloshnikov@users.noreply.github.com>
2024-08-14 19:51:14 +0300

    done

--- src/app/components/Testimonials.tsx
+++ src/app/components/Testimonials.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from "next/image";
 import avatar1 from "../assets/avatar-1.png";
 import avatar2 from "../assets/avatar-2.png";
@@ -9,6 +10,8 @@ import avatar7 from "../assets/avatar-7.png";
 import avatar8 from "../assets/avatar-8.png";
 import avatar9 from "../assets/avatar-9.png";
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
+import React from "react";
 
 const testimonials = [
   {
@@ -71,30 +74,47 @@ const firstColumn = testimonials.slice(0, 3);
 const secondColumn = testimonials.slice(3, 6);
 const thirdColumn = testimonials.slice(6, 9);
 
-const TestimonialsColumn = (props: {className?: string; testimonials: typeof testimonials}) => (
-<div className={twMerge("flex flex-col gap-6 mt-10 [mask-image:linear-gradient(to-bottom,transparent,black_25%,black_75%,transparent)]", props.className)}>
-                    {props.testimonials.map(testimonial => (
-                        <div
-                        className="card" 
-                        key={testimonial.username}
-                        >
-                            <div>{testimonial.text}</div>
-                            <div className="flex items-center gap-2 mt-5">
-                                <Image 
-                                src={testimonial.imageSrc} 
-                                alt={testimonial.name} 
-                                width={40}
-                                height={40}
-                                className="h-10 w-10 rounded-full"
-                                />
-                                <div className="flex flex-col">
-                                    <div className="font-medium tracking-tight leading-5">{testimonial.name}</div>
-                                    <div className="leading-5 tracking-tight">{testimonial.username}</div>
-                                </div>
-                            </div>
-                        </div>
+const TestimonialsColumn = (props: {className?: string; testimonials: typeof testimonials, duration: number}) => (
+  <motion.div 
+  animate={{
+    translateY: "-50%",
+  }}
+  transition={{
+    duration: props.duration || 10,
+    repeat: Infinity,
+    ease: 'linear',
+    repeatType: "loop",
+  }}
+  className={props.className}
+  >
+      <div className="flex flex-col gap-6 pb-6">
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <React.Fragment key={index}>
+                  {props.testimonials.map(testimonial => (
+                    <div
+                    className="card" 
+                    key={testimonial.username}
+                    >
+                    <div>{testimonial.text}</div>
+                      <div className="flex items-center gap-2 mt-5">
+                          <Image 
+                          src={testimonial.imageSrc} 
+                          alt={testimonial.name} 
+                          width={40}
+                          height={40}
+                          className="h-10 w-10 rounded-full"
+                          />
+                          <div className="flex flex-col">
+                              <div className="font-medium tracking-tight leading-5">{testimonial.name}</div>
+                              <div className="leading-5 tracking-tight">{testimonial.username}</div>
+                          </div>
+                      </div>
+                    </div>
                     ))}
-                </div>
+              </React.Fragment>
+            ))}
+      </div>
+  </motion.div>
 )
 
 export const Testimonials = () => {
@@ -110,10 +130,10 @@ export const Testimonials = () => {
                     From intuitive design to powerful features, our app has become an essential tool for users around the world
                 </p>
             </div>
-            <div className="flex justify-center gap-6">
-                <TestimonialsColumn testimonials={firstColumn}/>
-                <TestimonialsColumn testimonials={secondColumn} className="hidden md:flex"/>
-                <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:flex"/>
+            <div className="flex justify-center gap-6 mt-10 [mask-image:linear-gradient(to-bottom,transparent,black_25%,black_75%,transparent)] overflow-hidden">
+                <TestimonialsColumn testimonials={firstColumn} duration={15}/>
+                <TestimonialsColumn testimonials={secondColumn} className="hidden md:block" duration={19}/>
+                <TestimonialsColumn testimonials={thirdColumn} className="hidden lg:block" duration={17}/>
             </div>
         </div>
     </section>
